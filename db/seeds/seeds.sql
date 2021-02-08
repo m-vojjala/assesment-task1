9ecc7e40956f5fa86fbf6db488d863e3a8e544bd
m-vojjala <mounikavojjala@gmail.com>
2021-02-08 16:13:34 -0500

    database setup done

---
+++ db/seeds/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+  users(name, email, password)
+VALUES
+  ('John Doe', 'joh@ymail.com', 'password');
+
+INSERT INTO
+  users(name, email, password)
+VALUES
+  ('Amy Jones', 'amy@ymail.com', 'password');
+
+INSERT INTO
+  users(name, email, password)
+VALUES
+  ('Alice Blue', 'alice@gmail.com', 'password');
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
+ const { Pool } = require('pg');
+ require('dotenv').config();
+
+ const db = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+})
+db.connect().then(() => console.log('db conected'));
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get("/",(req,res)=>{
+app.get("/register",(req,res)=>{
   res.send("Hello")
 })
 
-app.listen(8080,()=>{
-  console.log(`server is running on port 8080`);
+app.listen(8990,()=>{
+  console.log(`server is running on port 8990`);
 });
